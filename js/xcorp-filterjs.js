e660a13179e001dc101c4ded157ac2165c5e0719
Alex Rondão <rondao@gmail.com>
2017-05-05 23:05:48 -0300

    estrutura do plugin mais completa

--- js/xcorp-filterjs.js
+++ js/xcorp-filterjs.js
@@ -1,1 +1,21 @@
 var teste = "Hello";
+
+(function($) {
+        $.fn.filter = function(options) {
+            // não há necessidade de fazer $ (this) porque
+            // "this" já é um objeto jquery
+
+            var defaults = {
+                "target": "div",
+                "typeStyle": "B", //-->> B - Bootstrap, M - Material
+                "data": [
+                    { "id": "", "type": "", "data": [], "displayName": "", "format": "", "placeholder": "", onBegin: function() {} }
+                ],
+                onApplied: function() {},
+                onRemoved: function() {}
+
+            };
+            var settings = $.extend({}, defaults, options);
+
+
+        })(jQuery);
